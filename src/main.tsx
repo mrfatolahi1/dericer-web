0e7b4c63ed561789c2767e569157174e1cdba6ad
mrfatolahi1 <fatolahi.cs@gmail.com>
2025-12-22 22:03:49 +0330

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./app/App";
+import App from "./styles/app/App";
 import { ThemeProvider } from "./styles/app/providers/ThemeProvider";
 import { QueryProvider } from "./styles/app/providers/QueryProvider";
 
